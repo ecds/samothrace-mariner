8bf0daf48a0664f5681aa23c05db4581e77e96e5
Sara Palmer <sepalme@emory.edu>
2015-06-08 12:03:10 -0400

    rest framework

--- samothrace/apps/sites/serializers.py
+++ samothrace/apps/sites/serializers.py
@@ -1,8 +1,20 @@
-from samothrace.apps.sites.models import Site
+from samothrace.apps.sites.models import Site, Marker, Koina
 from rest_framework import serializers
 
 class SiteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Site
-        fields = ('site_id', 'name', 'latitude', 'longitude', 'elevation')
+        fields = ('site_id', 'name', 'mod_name', 'alt_name', 'latitude', 'longitude', 'elevation', 'pleiades_url', 'perseus_url', 'caption')
+
+
+class MarkerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Marker
+        fields = ('marker_id', 'site', 'type',)
+
+
+class KoinaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Koina
+        fields = ('koina_id', 'site', 'inscription', 'member_count', 'activities', 'comments')
         
--- samothrace/urls.py
+++ samothrace/urls.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers, serializers, viewsets
-from samothrace.apps.sites.views import SiteViewSet
+from samothrace.apps.sites.views import SiteViewSet, MarkerViewSet, KoinaViewSet
+from samothrace.apps.inscriptions.views import InscriptionViewSet
+from samothrace.apps.people.views import IndividualViewSet, RoleViewSet, PriesthoodViewSet
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,6 +14,12 @@ admin.autodiscover()
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'sites', SiteViewSet)
+router.register(r'markers', MarkerViewSet)
+router.register(r'koina', KoinaViewSet)
+router.register(r'inscriptions', InscriptionViewSet)
+router.register(r'individuals', IndividualViewSet)
+router.register(r'roles', RoleViewSet)
+router.register(r'priesthoods', PriesthoodViewSet)
 
 
 urlpatterns = patterns('',
