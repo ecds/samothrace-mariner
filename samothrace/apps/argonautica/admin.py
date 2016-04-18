1541275d792b5c5ccb50160d4e1d00dcb02d4f54
Joanna <joanna@ecds-staffmac-01.library.emory.ddns>
2016-04-18 16:04:54 -0400

    Argo Admin

--- samothrace/apps/argonautica/admin.py
+++ samothrace/apps/argonautica/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from samothrace.apps.argonautica.models import Person, Stops, Places_Referenced
 from samothrace.apps.sites.models import Site
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tribe']
+    search_fields = ['name', 'tribe']
 
+class StopsAdmin(admin.ModelAdmin):
+    list_display = ['type_of_stop','line_number','ritual']
+    search_fields = ['type_of_stop','line_number','ritual']
+    
+class Places_ReferencedAdmin(admin.ModelAdmin):
+    list_display = ['type_of_reference','line_number','ritual']
+    search_fields = ['type_of_reference','line_number','ritual']
+    
 
-
-admin.site.register(Person)
-admin.site.register(Stops)
-admin.site.register(Places_Referenced)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Stops, StopsAdmin)
+admin.site.register(Places_Referenced, Places_ReferencedAdmin)
