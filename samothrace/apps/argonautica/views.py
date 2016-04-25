a1555af510c1088330e4cb3877efa28bf52ff862
Joanna <joanna@ecds-staffmac-01.library.emory.ddns>
2016-04-25 11:05:47 -0400

    View Edges Argo

--- samothrace/apps/argonautica/views.py
+++ samothrace/apps/argonautica/views.py
@@ -14,6 +14,9 @@ from django.template import RequestContext
 
 
 def Argo_Edges(request):
-    ArgoEdgeData = Stops.objects.values('id', 'crew__name', 'crew__origin__name', 'crew__origin__latitude', 'crew__origin__longitude', 'place_of_stop__name', 'place_of_stop__latitude', 'place_of_stop__longitude').filter(type_of_stop='direct')
-    return render_to_csv_response(ArgoEdgeData, field_header_map={'id': 'record_id', 'crew__name': 'crew_member_name', 'crew__origin__name': 'source', 'crew__origin__latitude': 'source_lat', 'crew__origin__longitude': 'source_long', 'place_of_stop__name': 'target', 'place_of_stop__latitude': 'target_lat', 'place_of_stop__longitude': 'target_long'})
+    ArgoEdgeData = Stops.objects.values('id', 'crew__name', 'crew__origin__site_id', 'crew__origin__name', 'crew__origin__latitude', 'crew__origin__longitude', 'place_of_stop__site_id', 'place_of_stop__name', 'place_of_stop__latitude', 'place_of_stop__longitude').filter(type_of_stop='direct')
+    return render_to_csv_response(ArgoEdgeData, field_header_map={'id': 'record_id', 'crew__name': 'crew_member_name', 'crew__origin__site_id': 'source_id', 'crew__origin__name': 'source', 'crew__origin__latitude': 'source_lat', 'crew__origin__longitude': 'source_long', 'place_of_stop__site_id': 'target_id', 'place_of_stop__name': 'target', 'place_of_stop__latitude': 'target_lat', 'place_of_stop__longitude': 'target_long'})
 
+
+#def map(request):
+#    return render(request, 'map.html')
